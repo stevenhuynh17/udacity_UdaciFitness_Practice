6a728bf464cd3328e632e9b948a5315e7f42f24b
Steven <stevenhuynh17@gmail.com>
2018-08-09 14:08:25 -0700

    add increment

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -3,6 +3,27 @@ import { View, Text } from 'react-native'
 import { getMetricMetaInfo } from '../utils/helpers'
 
 export default class AddEntry extends Component {
+  state = {
+    run: 0,
+    bike: 0,
+    swim: 0,
+    sleep: 0,
+    eat: 0
+  }
+
+  increment = (metric) => {
+    const { max, step } = getMetricMetaInfo(metric)
+
+    this.setState((state) => {
+      const count = state[metric] + step
+
+      return {
+        ...state,
+        [metric]: count > max ? max : count
+      }
+    })
+  }
+
   render() {
     return (
       <View>
