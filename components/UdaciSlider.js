c31e5a41384b3ec6053f84e8a81158cca71df40d
Steven <stevenhuynh17@gmail.com>
2018-08-18 17:02:55 -0700

    made sliders for sleep and eating

--- components/UdaciSlider.js
+++ components/UdaciSlider.js
@@ -1,10 +1,21 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Slider, StyleSheet, Text } from 'react-native'
 
-export default function UdaciSteppers() {
+export default function UdaciSteppers({ max, unit, step, value, onChange }) {
   return (
     <View>
-      <Text>UdaciSlider</Text>
+      <Slider
+        step={step}
+        value={value}
+        maximumValue={max}
+        minimumValue={0}
+        onValueChange={onChange}
+      />
+      <View>
+        <Text>{value}</Text>
+        <Text>{unit}</Text>
+      </View>
     </View>
+
   )
 }
