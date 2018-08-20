fd90ca0bb993c5e24a523af1af4019b303bbbd38
Steven <stevenhuynh17@gmail.com>
2018-08-20 15:00:51 -0700

    added async data storage

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
 import { View, TouchableOpacity, Text } from 'react-native'
 import { getMetricMetaInfo, timeToString } from '../utils/helpers'
+import { submitEntry, removeEntry } from '../utils/api'
+import { Ionicons } from '@expo/vector-icons'
+
 import UdaciSteppers from './UdaciSteppers'
 import UdaciSlider from './UdaciSlider'
 import DateHeader from './DateHeader'
+import TextButton from './TextButton'
 
 function SubmitBtn({ onPress }) {
   return (
@@ -60,6 +64,8 @@ export default class AddEntry extends Component {
     const key = timeToString()
     const entry = this.state
 
+    submitEntry({ key, entry})
+
     // Update Redux
     this.setState(() => {
       return {
@@ -72,8 +78,29 @@ export default class AddEntry extends Component {
     })
   }
 
+  reset = () => {
+    const key = timeToString()
+
+    removeEntry(key)
+  }
+
   render() {
     const metaInfo = getMetricMetaInfo()
+
+    if (this.props.alreadyLogged) {
+      return (
+        <View>
+          <Ionicons
+            name={'ios-happy-outline'}
+            size={100}
+          />
+          <Text>You already logged your infomration for today.</Text>
+          <TextButton onPress={this.reset}>
+            Reset
+          </TextButton>
+        </View>
+      )
+    }
 
     return (
       <View>
