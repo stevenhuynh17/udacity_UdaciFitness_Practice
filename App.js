fa4c617bc2db217a8d28bb2a21f049175171fb6e
Steven <stevenhuynh17@gmail.com>
2018-08-23 15:28:07 -0700

    history component functioning.

--- App.js
+++ App.js
@@ -4,13 +4,14 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import AddEntry from './components/AddEntry'
 import reducer from './reducers'
+import History from './components/History'
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
         <View style={{flex: 1}}>
-          <AddEntry />
+          <History/>
         </View>
       </Provider>
     )
